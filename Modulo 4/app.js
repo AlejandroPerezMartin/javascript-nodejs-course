a3264c879797e9188d97f155b5b8dc45c88fa8f9
Alejandro <aleperez92@gmail.com>
2015-05-30 17:26:36 +0200

    Errata and added html and body tag to response

--- Modulo 4/app.js
+++ Modulo 4/app.js
@@ -11,22 +11,22 @@ var express = require('express'),
 var port = 3000;
 
 app.get('/preguntas', function (req, res) {
-    res.send('<html><body><h2>Preguntas</h2><form action="/respuestas" method="get">' + '<input type="hidden" name="idpregunta" value="1">' + '<label for="respuesta">¿Quién descubrió América?</label> ' + '<input type="text" name="respuesta" placeholder="Tu respuesta...">' + '<input type="submit" name="submit" value="Enviar">' + '</form>' + '<form action="/respuestas" method="get">' + '<input type="hidden" name="idpregunta" value="2">' + '<label for="respuesta">¿Capital de Portugal?</label> ' + '<input type="text" name="respuesta" placeholder="Tu respuesta...">' + '<input type="submit" name="submit" value="Enviar">' + '</form><body></html>');
+    res.send('<html><body><h2>Preguntas</h2><form action="/respuestas" method="get">' + '<input type="hidden" name="idpregunta" value="1">' + '<label for="respuesta">¿Quién descubrió América?</label> ' + '<input type="text" name="respuesta" placeholder="Tu respuesta...">' + '<input type="submit" name="submit" value="Enviar">' + '</form>' + '<form action="/respuestas" method="get">' + '<input type="hidden" name="idpregunta" value="2">' + '<label for="respuesta">¿Capital de Portugal?</label> ' + '<input type="text" name="respuesta" placeholder="Tu respuesta...">' + '<input type="submit" name="submit" value="Enviar">' + '</form></body></html>');
 });
 
 app.get('/respuestas', function (req, res) {
 
-    var msg = "";
+    var msg = "<html><body>";
 
     if ((req.query.idpregunta == 1 && /crist[oó]bal col[oó]n/i.test(req.query.respuesta)) ||
         (req.query.idpregunta == 2 && /lisboa/i.test(req.query.respuesta))) {
-        msg = "<strong>Felicidades, has contestado correctamente a la respuesta.</strong>";
+        msg += "<strong>Felicidades, has contestado correctamente a la respuesta.</strong>";
     }
     else {
-        msg = "Lo sentimos, la respuesta correcta era <em>" + ((req.query.idpregunta == 1) ? 'Cristóbal Colón' : 'Lisboa') + '</em>.';
+        msg += "Lo sentimos, la respuesta correcta era <em>" + ((req.query.idpregunta == 1) ? 'Cristóbal Colón' : 'Lisboa') + '</em>.';
     }
 
-    res.send(msg + '<p><a href="/preguntas" title="Volver a la página inicial">Volver a la página inicial</a></p>');
+    res.send(msg + '<p><a href="/preguntas" title="Volver a la página inicial">Volver a la página inicial</a></p></body></html>');
 
 });
 
